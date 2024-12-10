735beb6e509df6322ed8f7347fc73a1f81938e29
jahangeer-44 <md.jahangeer143304@gmail.com>
2024-12-11 02:22:51 +0530

    first commit

---
+++ PrimeNumbers.java
@@ -0,0 +1,19 @@
+// One_to_Hundred(PrimeNumbers).java
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers between 1 and 100 are:");
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
